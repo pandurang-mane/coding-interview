a7038730a87f8ce1fba476da84a490889765f592
Pandurang Mane <pandurangmane654@gmail.com>
2024-01-11 16:43:20 +0530

    Balanced Parenthesis code added

---
+++ src/com/pandurang/BalancedParenthesis.java
@@ -0,0 +1,34 @@
+package com.pandurang;
+
+public class BalancedParenthesis {
+
+
+    public static void main(String[] args) {
+        boolean isBalanced = checkBalancedParenthesis("{[(])}");
+        System.out.println(isBalanced);
+    }
+
+    private static boolean checkBalancedParenthesis(String s) {
+        Stack<Character> stack = new Stack<>(s.length());
+        boolean isBalanced = false;
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if(('(' == (charArray[i])) || ('[' == (charArray[i])) || ('{' == (charArray[i]))) {
+                stack.push(charArray[i]);
+            } else {
+                if((charArray[i] == ')') && (stack.pop() == '(')) {
+                    isBalanced = true;
+                } else if((charArray[i] == ']') && (stack.pop() == '[')) {
+                    isBalanced = true;
+                } else if((charArray[i] == '}') && (stack.pop() == '{')) {
+                    isBalanced = true;
+                } else {
+                    isBalanced = false;
+                    return isBalanced;
+                }
+            }
+        }
+        return isBalanced;
+    }
+
+}
